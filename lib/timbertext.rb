f68488522179b88d138fce07da6a32a0b4a6c432
Bryan T. Meyers <btmeme@rit.edu>
2015-07-30 10:59:43 -0400

    ADDED: proper parsing of linebreaks and paragraphs

--- lib/timbertext.rb
+++ lib/timbertext.rb
@@ -7,6 +7,8 @@ ORDERED_ITEM = /^\(#\) (.*\n(?:(?!\(#\)).*\n|\n*)*)/
 TABLE = /((?:(?:\|[^|\n]*)+\|\n)*)(?:(?:\|[\- ]*)+\|\n)((?:(?:\|[^|\n]*)+\|\n)*)/
 TABLE_ROW = /((?:\|[^|\n]*)*)\|\n/
 TABLE_CELL = /\|([^|\n]+)/
+PARAGRAPH = /(^(?:(?!\||\(.\)|=\w=|[ ]{4}|\t).+\n)+)/
+BREAK = /(\\\\)\n[ \t]*/
 
 module TimberText
 
@@ -23,26 +25,36 @@ module TimberText
   end
 
   def self.parse( text , level = 1)
+    text.gsub!(PARAGRAPH) do
+      "<p>#{rinse_repeat($1.gsub(/(?!^|\\\\)\n(?!=\n)/, ' '),level)}</p>\n"
+    end
+    text.gsub!(BREAK , "</br>")
     text.gsub!(TAG_GROUP) do
       case $1
         when 'c'
-          $2.gsub(HEADER , "<pre><code#{$1 ? " class=\"#{$1}\"" : ''}>\n" + rinse($2) + '</code></pre>' )
+          $2.gsub(HEADER) do
+            "<pre><code#{$1 ? " class=\"#{$1}\"" : ''}>\n" + rinse($2) + '</code></pre>'
+          end
         when '1','2','3','4','5','6'
           level = $1.to_i
-          $2.gsub(HEADER , "<h#{level}>#{$1}</h#{level}>" + rinse_repeat($2,level) )
+          $2.gsub(HEADER) do
+            "<h#{level}>#{$1}</h#{level}>\n" + rinse_repeat($2,level)
+          end
         when 'h'
-          $2.gsub(HEADER , "<h#{level}>#{$1}</h#{level}>" + rinse_repeat($2,level) )
+          $2.gsub(HEADER) do
+            "<h#{level}>#{$1}</h#{level}>\n" + rinse_repeat($2,level)
+          end
         when 'p'
-          '<p>' + rinse_repeat($2,level) + '</p>'
+          '<p>' + rinse_repeat($2,level) + '</p>\n'
         when 'q'
-          '<blockquote>' + rinse_repeat( $2 , level) + '</blockquote>'
+          '<blockquote>' + rinse_repeat( $2 , level) + '</blockquote>\n'
         else
           ''
       end
     end
     text.gsub!(LIST_GROUP) do
       tag = $2.eql?('*') ? 'ul' : 'ol'
-      match = $2.eql?( '*') ? UNORDERED_ITEM : ORDERED_ITEM
+      match = $2.eql?('*') ? UNORDERED_ITEM : ORDERED_ITEM
       "<#{tag}>" + $1.gsub(match) do
         '<li>' + rinse_repeat( $1, level + 1) + '</li>'
       end + "</#{tag}>"
@@ -57,6 +69,10 @@ module TimberText
       end + '</tbody></table>'
     end
     text
+  end
+
+  def self.build( text )
+    parse(text , 1)
   end
 end
 
@@ -85,7 +101,7 @@ HEADER_TEST = '    =h= Another H1
 =h= another group
 (*) why?
     also a thing
-(*) still a bullet
+(*) still a bullet\\\\
     this is strill a thiing
     (#) this is is ok tooo
     (#) hello there
@@ -93,4 +109,4 @@ HEADER_TEST = '    =h= Another H1
 (*) yet another thing
 '
 
-puts TimberText.parse( HEADER_TEST , 1)
+puts TimberText.build( HEADER_TEST )
